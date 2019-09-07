6460151555eddd4aaebb44fef218e32fbd141ac9
Guido Queiroz <Takzzg@hotmail.com>
2019-09-07 00:55:59 -0300

    button-&-MyTextInput-+-vectorIcons-implementation

---
+++ src/components/Button/Button.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { View, TouchableOpacity, Text } from 'react-native';
+import FontistoIcon from 'react-native-vector-icons/Fontisto';
+import styles from './styles';
+
+const Button = ({
+  text,
+  onPress,
+  iconStyle,
+  textStyle,
+  containerStyle,
+  textContainerStyle,
+  icon: { iconName = '', iconSize = 25, iconColor = 'black' },
+}) => {
+  return (
+    <TouchableOpacity onPress={onPress} style={{ ...styles.container, ...containerStyle }}>
+      {iconName ? (
+        <View style={{ ...styles.icon, ...iconStyle }}>
+          <FontistoIcon size={iconSize} name={iconName} color={iconColor} />
+        </View>
+      ) : null}
+      {text ? (
+        <View style={{ ...styles.textContainer, ...textContainerStyle }}>
+          <Text style={textStyle}>{text}</Text>
+        </View>
+      ) : null}
+    </TouchableOpacity>
+  );
+};
+
+Button.propTypes = {
+  text: PropTypes.string,
+  onPress: PropTypes.func.isRequired,
+  iconStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
+  textStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
+  containerStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
+  textContainerStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
+  icon: PropTypes.shape({
+    iconName: PropTypes.string,
+    iconSize: PropTypes.number,
+    iconColor: PropTypes.string,
+  }),
+};
+
+Button.defaultProps = {
+  text: null,
+  iconStyle: {},
+  textStyle: {},
+  containerStyle: {},
+  textContainerStyle: {},
+  icon: {},
+};
+
+export default Button;
--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,45 +1,118 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Text, ScrollView, StyleSheet, ActivityIndicator } from 'react-native';
+import {
+  Text,
+  ScrollView,
+  StyleSheet,
+  // ActivityIndicator,
+  View,
+} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 import { goBack } from '../../navigation/Navigation';
 import { fetchCocktails } from '../../actions/cocktails';
-import Button from '../../components/Button';
+import Button from '../../components/Button/Button';
+import MyTextInput from '../../components/MyTextInput/MyTextInput';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 25,
   },
   center: {
     alignItems: 'center',
-    justifyContent: 'center'
-  }
+    justifyContent: 'center',
+  },
+  searchContainer: {
+    alignSelf: 'flex-start',
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  TextInput: {
+    margin: 5,
+  },
 });
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputText: '',
+    };
+  }
+
   render() {
-    const {
-      cocktails,
-      fetchCocktailsError,
-      fetchCocktailsIsLoading,
-      fetchCocktailsConnected,
-    } = this.props;
+    // const {
+    //   cocktails,
+    //   fetchCocktailsError,
+    //   fetchCocktailsIsLoading,
+    //   fetchCocktailsConnected,
+    // } = this.props;
+    const { inputText } = this.state;
+
+    const changeTextHandler = newText => {
+      this.setState({ inputText: newText });
+    };
+
+    const cancelButtonHandler = () => {
+      return this.setState({ inputText: '' });
+    };
 
     return (
-      <ScrollView style={styles.container} contentContainerStyle={styles.center} >
-        <Text>Home</Text>
-        <Button onPress={() => goBack()} text="Go back" />
+      <LinearGradient
+        style={styles.container}
+        colors={['#ff3300', '#ff1ac6']}
+        start={{ x: 0, y: 1 }}
+        end={{ x: 1, y: 0 }}
+      >
+        <View style={styles.searchContainer}>
+          {!inputText.length > 0 ? (
+            <Button
+              onPress={() => goBack()}
+              icon={{ iconName: 'arrow-left', iconColor: 'grey' }}
+              containerStyle={styles.backArrow}
+            />
+          ) : null}
 
-        {fetchCocktailsIsLoading ? (
-          <ActivityIndicator color="black" size="large" />
-        ) : (
-          <Button onPress={fetchCocktailsConnected} text="Fetch cocktails" />
-        )}
+          <MyTextInput
+            onChangeText={newText => changeTextHandler(newText)}
+            value={inputText}
+            containerStyle={styles.TextInput}
+            placeholder="Search"
+            icon
+          />
 
-        {cocktails ? <Text>{JSON.stringify(cocktails, null, 2)}</Text> : null}
-        {fetchCocktailsError ? (
-          <Text style={{ color: 'red' }}>{JSON.stringify(fetchCocktailsError, null, 2)}</Text>
-        ) : null}
-      </ScrollView>
+          {inputText.length > 0 ? (
+            <Button
+              onPress={cancelButtonHandler}
+              text="Cancel"
+              textStyle={{ color: 'red', fontWeight: 'bold', fontSize: 17 }}
+              containerStyle={styles.cancelButton}
+            />
+          ) : null}
+        </View>
+
+        <ScrollView style={styles.container} contentContainerStyle={styles.center}>
+          <Text>asd</Text>
+        </ScrollView>
+      </LinearGradient>
+
+      // <ScrollView style={styles.container} contentContainerStyle={styles.center}>
+      //   <Text>Home</Text>
+      //   <Button onPress={() => goBack()} text="Go back" />
+
+      //   {fetchCocktailsIsLoading ? (
+      //     <ActivityIndicator color="black" size="large" />
+      //   ) : (
+      //     <Button onPress={fetchCocktailsConnected} text="Fetch cocktails" />
+      //   )}
+
+      //   {cocktails ? <Text>{JSON.stringify(cocktails, null, 2)}</Text> : null}
+      //   {fetchCocktailsError ? (
+      //     <Text style={{ color: 'red' }}>{JSON.stringify(fetchCocktailsError, null, 2)}</Text>
+      //   ) : null}
+      // </ScrollView>
     );
   }
 }
